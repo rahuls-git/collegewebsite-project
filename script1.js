4099766de87ef78071763b007ab61c9cc7b67d5e
Rahul Kumar Singh <rahulkr4112@gmail.com>
2017-05-09 18:40:49 +0530

    collegewebsite
    A website for the college GB Pant Governmnet Engineering College

---
+++ script1.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+	    $('#0').click(function() {
+    $('#about').show();
+	$('#message').hide();
+	$('#canteen').hide();
+    $('#library').hide();
+	$('#sports').hide();
+	$('#contact').hide();
+	$('#flash').hide();
+});
+ $('#3').click(function() {
+    $('#message').show();
+	$('#about').hide();
+	$('#canteen').hide();
+    $('#library').hide();
+	$('#sports').hide();
+	$('#contact').hide();
+	$('#flash').hide();
+	
+});
+$('#10').click(function() {
+	$('#canteen').show();
+	$('#about').hide();
+	$('#message').hide();
+	$('#library').hide();
+	$('#sports').hide();
+	$('#contact').hide();
+	$('#flash').hide();
+});
+$('#11').click(function() {
+	$('#library').show();
+	$('#about').hide();
+	$('#message').hide();
+	$('#canteen').hide();
+	$('#sports').hide();
+	$('#contact').hide();
+	$('#flash').hide();
+});
+$('#12').click(function() {
+	$('#sports').show();
+    $('#message').hide();
+	$('#about').hide();
+	$('#canteen').hide();
+    $('#library').hide();
+	$('#contact').hide();
+	$('#flash').hide();
+	
+});
+$('#13').click(function() {
+	$('#contact').show();
+	$('#sports').hide();
+    $('#message').hide();
+	$('#about').hide();
+	$('#canteen').hide();
+    $('#library').hide();
+	$('#flash').hide();
+	
+});
+});
